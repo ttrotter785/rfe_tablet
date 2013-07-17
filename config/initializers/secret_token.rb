2773b18f505f8e606644ad9e586eb05f75b081a3
Tyson Trotter <ttrotter@leafsoftwaresolutions.com>
2013-07-17 16:02:41 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RfeTablet::Application.config.secret_token = 'e50880f34d9efe1ef337c6f5ebcd46b3bb8e742452437154bd0b8f6964e506d3f24feb556edf6ba5c7f9eea3f95f96fc1308dd497bdc73135633b73ccc058e4b'
